6f30b4dc0a6f38b449502763d577aa9f3ed741c2
nadydev <105218942+nadydev@users.noreply.github.com>
2023-02-10 02:54:40 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -42,3 +42,51 @@ var swiper = new Swiper(".trending-content", {
       },
     },
   });
+
+  // Scroll Bar 
+  window.onscroll = function() {
+    scrollAction()
+  };
+
+  function scrollAction(){
+    var winscroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winscroll / height) * 100 ;
+    document.getElementById('scroll-bar').style.width =scrolled + '%';
+  }
+
+
+  var flag = true;
+
+
+  // function changeColor() {
+  //     if (flag) {
+  //         r.style.setProperty('--main-color', '#0d81ec');//blue colour
+  //        document.getElementById("color").style.backgroundColor="#0d81ec"
+  //         flag = false;
+  //     }
+  //     else {
+  //         r.style.setProperty('--main-color', '#fa5353'); //The main colour
+  //         document.getElementById("color").style.backgroundColor="#fa5353"
+  //         flag = true;
+  //     }
+  // }
+  
+  var root = document.querySelector(':root');
+  function changeColor(){
+
+    // while(true){
+  
+
+      var r =Math.round(Math.random()*255);
+      var g =Math.round(Math.random()*255);
+      var b =Math.round(Math.random()*255);
+      var randomColor = `rgba( ${r}, ${g}, ${b})`;
+      root.style.setProperty('--main-color', randomColor);//blue colour
+
+      // var a =Math.random()
+   document.getElementById("color").style.backgroundColor= randomColor;
+  
+    
+  }
+  // }
